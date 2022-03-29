dc6db2828f7787dda0ba6490053973716feff245
voidbringer666 <100240410+voidbringer666@users.noreply.github.com>
2022-03-30 00:42:41 +0300

    Update app.js

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -6,6 +6,24 @@ class App{
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
     
+        this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth/window.innerHeight, 0.1, 100);
+        this.camera.position.set(0,0,4);
+        
+        this.scene = new THREE.scene();
+        this.scene.background = new THREE.Color( 0xaaaaaa );
+        
+        this.renderer = new THREEWebGLRenderer({ antialias:true } );
+        this.renderer.setPixelRatio( window.devicePixelRatio );
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
+        container.appendChild( this.renderer.domElement);
+        
+        this.renderer.set.AnimationLoop( this.render.bind(this));
+        
+        const geometry = new THREE.BoxBufferGeometry();
+        const material = new THREE.MeshStandartMaterial({ color: 0xff0000 });
+        
+        this.mesh = new THREE.mesh( geometry, material );
+        this.scene.add( this.mesh );
         
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
@@ -15,7 +33,8 @@ class App{
     }
     
 	render( ) {  
-        
+        this.mesh.rotateY( 0.01 );
+        this.renderer.render(this.scene, this.camera)
     }
 }
 
